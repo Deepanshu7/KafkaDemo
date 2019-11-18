f95229017a6dd7d377d87489da458e8d7c0da594
Deepanshu7 <chawladeepanshu7@gmail.com>
2019-11-18 08:49:18 +0530

    Add files via upload

---
+++ kafka-demo/kafka-consumer/src/main/java/com/dips/kafkaconsumer/KafkaConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.dips.kafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.dips.*")
+public class KafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+
+}
